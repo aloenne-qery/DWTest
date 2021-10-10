9bfae5eee9215aaeb40378f1e28cadaa1f9bcc5b
aloenne <63609186+aloenne@users.noreply.github.com>
2021-10-10 16:46:28 +0200

    Create ssb_test1.py

---
+++ ssb_test1.py
@@ -0,0 +1,36 @@
+pip install pyjstat
+from pyjstat import pyjstat
+import requests
+ssburl = 'https://data.ssb.no/api/v0/no/table/06913/'
+query = {
+  "query": [
+    {
+      "code": "Region",
+      "selection": {
+        "filter": "vs:Landet",
+        "values": [
+          "0"
+        ]
+      }
+    },
+    {
+      "code": "ContentsCode",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "Folkemengde"
+        ]
+      }
+    }
+  ],
+  "response": {
+    "format": "json-stat2"
+  }
+}
+resultat = requests.post(ssburl, json = query)
+print(resultat)
+dataset = pyjstat.Dataset.read(resultat.text)
+type(dataset)
+df = dataset.write('dataframe')
+df.info
+df.to_csv('befolkning.csv', index=False)
